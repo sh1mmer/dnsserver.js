766f993b5a4ef2a19b1b97910eb433cd90dd71b9
Tom Hughes-Croucher <tom_croucher@yahoo.com>
2010-07-14 18:47:34 -0700

    sucessfully chopping up a request into an object

--- dnsserver.js
+++ dnsserver.js
@@ -5,17 +5,17 @@ var sys = require('sys'),
 host = "localhost";
 port = 9999;
 
-//slices a single byte into bits
+// slices a single byte into bits
+// assuming only single bytes
 var sliceBits = function(b, off, len) {
-    mod = 7 + ((b.length - 1) * 8);
-    len = start - len
-    var s = mod - (off + len - 1);
+    var s = 7 - (off + len - 1);
 
     b = b >>> s;
     return b & ~(0xff << len);
 };
 
-//pulls bits out of bytes
+// pulls bits out of bytes
+// assuming you cannot cross byte boundaries for now
 var sliceBytes  = function(buf, startBits, endBits) {
 
     var startOffset = startBits % 8;
@@ -36,10 +36,6 @@ var sliceBytes  = function(buf, startBits, endBits) {
         
         //slice down to the new size
         var newBuf = buf.binarySlice(startBytes, endBytes);
-        //012345678
-        //04
-        //128  64  32  16   8   4   2   1
-        //0    0   0   0    0   0   0   1
         
         var str = newBuf.toString('binary', 0, newBuf.length);
         console.log("buf len: " + newBuf.length);
@@ -56,14 +52,11 @@ var server = dgram.createSocket(function (msg, rinfo) {
     //console.log("server got: " + sys.inspect(msg));
     
     //var msgStr = sliceBits(msg,0,47);
-    sliceBytes(msg, 1,2);
-    console.log(sys.inspect(msg));
-    console.log(msg.length);
-    console.log(sys.inspect(msg.toString('binary')));
-    console.log(msg.toString('binary').length);
     
     //split up the message into the dns request header info and the query
-    //console.log(sys.inspect(processRequest(msg)));
+    var q = processRequest(msg);
+    console.log(sys.inspect(q));
+    console.log(sys.inspect(q.question.qname.toString('binary')));
 });
 
 //takes a buffer as a request
@@ -75,43 +68,56 @@ var processRequest = function(req) {
     query.header = {};
     //TODO write code to break questions up into an array
     query.question = {};
-    
+
+    var tmpSlice;
+    var tmpByte;
+        
     //transaction id
     // 2 bytes
     query.header.transId = req.slice(0,2);
 
-    //slice out 2 bytes for the next section to dice into binary.
-    var tmpSlice1 = req.slice(2,3);
+    //slice out a byte for the next section to dice into binary.
+    tmpSlice = req.slice(2,3);
+    //convert the binary buf into a string and then pull the char code
+    //for the byte
+    tmpByte = tmpSlice.toString('binary', 0, 1).charCodeAt(0);
+    
     //qr
     // 1 bit
-    query.header.qr = tmpSlice1.slice(0,1);
+    query.header.qr = sliceBits(tmpByte, 0,1);
     //opcode
     // 0 = standard, 1 = inverse, 2 = server status, 3-15 reserved
     // 4 bits
-    query.header.opcode = tmpSlice.slice(1,5);
+    query.header.opcode = sliceBits(tmpByte, 1,4);
     //authorative answer
     // 1 bit
-    query.header.aa = tmpSlice.slice(5,21);
+    query.header.aa = sliceBits(tmpByte, 5,1);
     //truncated
     // 1 bit
-    query.header.tc = req.slice(22,22);
+    query.header.tc = sliceBits(tmpByte, 6,1);
     //recursion desired
     // 1 bit
-    query.header.rd = req.slice(23,23);
+    query.header.rd = sliceBits(tmpByte, 7,1);
+
+    //slice out a byte to dice into binary
+    tmpSlice = req.slice(3,4);
+    //convert the binary buf into a string and then pull the char code
+    //for the byte
+    tmpByte = tmpSlice.toString('binary', 0, 1).charCodeAt(0);
+    
     //recursion available
     // 1 bit
-    query.header.ra = req.slice(24,24);
+    query.header.ra = sliceBits(tmpByte, 0,1);
+
     //reserved 3 bits
+    query.header.z = sliceBits(tmpByte, 1,3);
 
-    var tmpSlice2 = req.slice(3,4);
-
-    query.header.z = req.slice(25,27);
     //response code
     // 0 = no error, 1 = format error, 2 = server failure
     // 3 = name error, 4 = not implemented, 5 = refused
     // 6-15 reserved
     // 4 bits
-    query.header.rcode = req.slice(28,32);
+    query.header.rcode = sliceBits(tmpByte, 4,4);
 
     //question count
     // 2 bytes
@@ -130,11 +136,11 @@ var processRequest = function(req) {
     //qname is the sequence of domain labels
     //qname length is not fixed however it is 4
     //octets from the end of the buffer
-    query.question.qname = req.slice(97, req.length - 33);
+    query.question.qname = req.slice(12, req.length - 4);
     //qtype
-    query.question.qtype = req.slice(req.length - 32, req.length - 17);
+    query.question.qtype = req.slice(req.length - 4, req.length - 2);
     //qclass
-    query.question.qclass = req.slice(req.length - 16, req.length -1);
+    query.question.qclass = req.slice(req.length - 2, req.length);
     
     return query;
 };
